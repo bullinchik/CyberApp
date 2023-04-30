55c9b3dc64b236c92ab296221f9d3ca4e3de8f33
bullinchik <happysonphil@gmail.com>
2023-05-01 00:36:12 +0300

    add listview

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,23 +1,41 @@
-﻿namespace CyberApp;
-
+﻿using CyberApp.Models;
+namespace CyberApp;
 public partial class MainPage : ContentPage
 {
-    int count = 0;
+    public List<User> Users { get; set; }
 
     public MainPage()
     {
         InitializeComponent();
-    }
-
-    private void OnCounterClicked(object sender, EventArgs e)
-    {
-        count++;
-
-        if (count == 1)
-            CounterBtn.Text = $"Clicked {count} time";
-        else
-            CounterBtn.Text = $"Clicked {count} times";
-
-        SemanticScreenReader.Announce(CounterBtn.Text);
+        Label header = new Label { Text = "Список пользователей", FontSize = 18 };
+        Users = new List<User>
+        {
+            new User {Name="Tom", Age=38 },
+            new User {Name = "Bob", Age= 42},
+            new User {Name="Sam", Age = 28},
+            new User {Name = "Alice", Age = 33}
+        };
+        
+        ViewCharacters.ItemsSource = Users;
+        ViewCharacters.ItemTemplate = new DataTemplate(() =>
+        {
+            // привязка к свойству Name
+            Label nameLabel = new Label { FontSize = 16 };
+            nameLabel.SetBinding(Label.TextProperty, "Name");
+ 
+            // привязка к свойству Age
+            Label ageLabel = new Label { FontSize = 14 };
+            ageLabel.SetBinding(Label.TextProperty, "Age");
+ 
+            return new ViewCell
+            {
+                View = new StackLayout
+                {
+                    Padding = new Thickness(0, 5),
+                    Orientation = StackOrientation.Vertical,
+                    Children = { nameLabel, ageLabel }
+                }
+            };
+        });
     }
 }
